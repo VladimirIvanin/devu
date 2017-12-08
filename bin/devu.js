9efa1ebff896bda32292b7998ec58625746f1f81
VladimirIvanin <ivaninvv@mail.ru>
2017-12-09 02:47:20 +0300

    0.11.0

--- bin/devu.js
+++ bin/devu.js
@@ -2,6 +2,7 @@
 var Liftoff = require('liftoff');
 var argv = require('minimist')(process.argv.slice(2));
 var InsalesUploader = require('insales-uploader');
+var Promise = require('promise');
 
 var devu = new Liftoff({
   name: 'devu',
@@ -22,57 +23,38 @@ var devu = new Liftoff({
   console.log('Respawned to PID:', child.pid);
 });
 
-/**
- * devu --make --template index
- * devu --make --snippets logo,cart_widget,nav
- */
 var invoke = function (env) {
   var config =  {};
   if (env.configPath) {
     config = require(env.configPath);
   }
-  var _root = env.cwd;
+  if (!config.uploader) {
+    console.log('Нет настроек для insales-uploader')
+    return;
+  }
 
-  if (argv.stream || argv.download || argv.upload || argv.pull || argv.push || argv.start) {
-    if (!config.uploader) {
-      console.log('Нет настроек для insales-uploader')
-      return;
+  if (config.uploader.usePostCss) {
+    if (!config.uploader.tools) {
+      config.uploader.tools = {};
     }
-    if (config.uploader.usePostCss) {
-      if (!config.uploader.tools) {
-        config.uploader.tools = {};
+    config.uploader.tools.postCssPlugins = postCssPlugins;
+  }
+
+  var uploader = new InsalesUploader(config.uploader);
+
+  var methods = [];
+  if (argv['_'].length == 0) {
+    methods.push(uploader.start)
+  }else {
+    for (var i = 0; i < argv['_'].length; i++) {
+      var method = argv['_'][i];
+      if (alias[method]) {
+        methods.push(uploader[alias[method]]());
       }
-
-      config.uploader.tools.postCssPlugins = [
-        require('postcss-nested')(),
-        require('postcss-flexbugs-fixes')(),
-        require('postcss-discard-duplicates')(),
-        require('postcss-discard-empty')(),
-        require('postcss-ordered-values')(),
-        require('postcss-combine-duplicated-selectors')(),
-        require('stylefmt')()
-      ]
-    }
-    var uploader = new InsalesUploader(config.uploader);
-    if (argv.start) {
-      uploader.start()
-    }
-    if (argv.stream) {
-      uploader.stream()
-    }
-    if (argv.download) {
-      uploader.download()
-    }
-    if (argv.upload) {
-      uploader.upload()
-    }
-    if (argv.pull) {
-      uploader.pullTheme()
-    }
-    if (argv.push) {
-      uploader.pushTheme()
     }
   }
+
+  Promise.all[methods];
 };
 
 devu.launch({
@@ -81,3 +63,23 @@ devu.launch({
   require: argv.require,
   completion: argv.completion
 }, invoke);
+
+var postCssPlugins = [
+  require('postcss-nested')(),
+  require('postcss-flexbugs-fixes')(),
+  require('postcss-discard-duplicates')(),
+  require('postcss-discard-empty')(),
+  require('postcss-ordered-values')(),
+  require('postcss-combine-duplicated-selectors')(),
+  require('stylefmt')()
+]
+
+var alias = {
+  start: 'start',
+  stream: 'stream',
+  download: 'download',
+  upload: 'upload',
+  pull: 'pullTheme',
+  assets: 'initAssets',
+  push: 'pushTheme'
+}
